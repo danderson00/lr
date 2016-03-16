4b31c6d60aad5b54eb03dce7ecbf7cf8264a82f3
Dale Anderson <dalander@microsoft.com>
2016-03-16 16:33:59 -0700

    Removed console.log calls, added storage account details

--- api/acceptfriendrequest.js
+++ api/acceptfriendrequest.js
@@ -2,21 +2,21 @@ var wrap = require('azure-mobile-apps-compatibility').wrap;
 
 module.exports = wrap.api(function (exports, statusCodes) {
 exports.post = function(request, response) {
-    console.log(request.body);
-    console.log('request from: ', request.body.fromUserId);
+    //console.log(request.body);
+    //console.log('request from: ', request.body.fromUserId);
     // response.send(200, { Status : 'SUCCESS'});
-    
+
     //Scenario:
     //Friend A has requested to be friends with Friend B
     //Friend B has accepted the request via the RocketList
-    
+
     //Update Friends record for record from A to B
     var friendsTable = request.service.tables.getTable('Friends');
     var rocket = request.body;
-    
+
     friendsTable.where(function(rocket) {
         return this.fromUserId == rocket.fromUserId && this.toUserId == rocket.toUserId;
-    },rocket).read({ 
+    },rocket).read({
 		success: function(results) {
             if (results.length === 0) {
 				response.send(200, { Status : "FAIL", Error : "Sorry!  Couldn't find friend request"});
@@ -34,7 +34,7 @@ exports.post = function(request, response) {
                     createDate:  new Date(),
                     updateDate:  new Date(),
                     requestedBy: friendRequest.toUserId,
-                    toUsername:  rocket.fromUsername                     
+                    toUsername:  rocket.fromUsername
                 };
                 friendsTable.insert(newFriendRequest);
                 //Update Rockets list to note friend request has been seen
@@ -45,7 +45,7 @@ exports.post = function(request, response) {
                         response.send(200, { Status : "Success", UpdatedId: rocket.id });
                     }, error: function(error) {
                         console.error("Couldn't update message: ", error);
-                        response.send(200, { Status : "FAIL", Error : "Sorry!  Couldn't update Rocket"});            
+                        response.send(200, { Status : "FAIL", Error : "Sorry!  Couldn't update Rocket"});
                     }
                 });
             }
@@ -53,7 +53,7 @@ exports.post = function(request, response) {
             console.error("Couldn't connect friends: ", error);
             response.send(200, { Status : "FAIL", Error : "Sorry!  Couldn't connect friends"});
         }
-    });    
+    });
 };
 
 })
